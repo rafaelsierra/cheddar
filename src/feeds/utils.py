a288818f7d7222891f74eb344cf41e37210de881
Rafael Sierra <rafael.sierra@sabbre.com.br>
2013-04-10 20:39:44 -0300

    Decreased tasks granularity

--- src/feeds/utils.py
+++ src/feeds/utils.py
@@ -15,7 +15,9 @@ def build_request(url):
 
 def feedopen(url):
     request = build_request(url)
+    # TODO: This is not working as I expected. I'm having lots of worker 
+    # deadlocks and I think this granularity is the cause
     # http://docs.celeryproject.org/en/latest/userguide/tasks.html#avoid-launching-synchronous-subtasks
-    chain = make_request.s(request)|parse_feed.s()
-    feed = chain().get()    
+    #chain = make_request.s(request)|parse_feed.s()
+    feed = parse_feed(make_request(request))
     return feed
