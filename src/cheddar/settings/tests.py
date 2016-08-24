6b831207f17992a0dfaf3d7a3c2b374e62353c04
Rafael Sierra <rafaeljsg14@gmail.com>
2016-08-24 22:02:45 +0200

    Added .travis.yml

---
+++ src/cheddar/settings/tests.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Django settings for a Dockerized cheddar
+from .default import *  # NOQA
+import djcelery
+
+USE_X_FORWARDED_HOST = True
+ALLOWED_HOST = ['*']
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '',
+    }
+}
+
+SECRET_KEY = 'something to test'
+
+djcelery.setup_loader()
